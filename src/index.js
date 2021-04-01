e84cc3ed7687261b158a3707f55f0714aa83ab82
Krzysiek Swol <64749798+krzysiekswol@users.noreply.github.com>
2021-04-01 16:34:55 +0200

    add banners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import style from "./css/index.scss";
+
+const slider = document.querySelector(".slider");
+const dotOne = document.querySelector(".one");
+const dotTwo = document.querySelector(".two");
+const dotThree = document.querySelector(".three");
+
+dotOne.addEventListener("click", function () {
+  if (dotOne.classList.contains("active")) {
+    slider.style.backgroundImage = `url("/src/assets/nike_slider.jpg")`;
+  } else {
+    dotOne.classList.add("active");
+  }
+});
+dotTwo.addEventListener("click", function () {
+  if (dotTwo.classList.contains("active")) {
+    slider.style.backgroundImage = `url("/src/assets/nike_slider.jpg")`;
+    dotTwo.style.backgroundColor = `red`;
+  } else {
+    dotTwo.classList.add("active");
+    slider.style.backgroundImage = `url("/src/assets/nike_slider.jpg")`;
+  }
+});
